3eafcf10a8ec2ee72b0671a3e3c1b8944e25cb0a
Artur Boettger <121630222+artur-boettger@users.noreply.github.com>
2023-09-15 16:49:22 -0300

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,20 +1,20 @@
 export const educationData = [
     {
-        id: 1,
+        id: 2,
         institution: 'Colegio São José',
         course: 'Base Educacional',
         startYear: '2008',
         endYear: '2019'
     },
     {
-        id: 2,
+        id: 3,
         institution: 'Eem Prof Henrique Da Silva Fontes',
         course: 'Ensino Médio',
         startYear: '2020',
         endYear: '2022'
     },
     {
-        id: 3,
+        id: 1,
         institution: 'Univali',
         course: 'Sistemas para Internet',
         startYear: '2023',
