57f3cfd41229bd32eba8cbfc1e502e425d381457
artur-boettger <arturboettger04@gmail.com>
2023-11-13 09:23:48 -0300

    correção

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -13,7 +13,7 @@ export const experienceData = [
         jobtitle: 'Desenvolvedor Full Stack',
         startYear: '2023',
         endYear: 'Atualmente',
-        description: 'Trabalhei em diferentes áreas da empresa, iniciando no faturamento, em seguida fui alocado para um armazém, onde eu fazia toda a parte de recepção, conferência e expedição de mercadorias, trabalhando com sistemas ERP, planilhas e Trello para a organização das tarefas.'
+        description: 'Na GeoSapiens, desempenho a função de desenvolvedor no produto Coletum, onde minha atuação abrange o aprimoramento da interface, implementação de funcionalidades, integração de APIs e definição de rotas. Possuo experiência nas seguintes tecnologias: Git/GitHub para controle de versão, PHP/Symfony/Twig para o desenvolvimento back-end, JS/Angular/React para a construção de interfaces dinâmicas, Postman para testes e Docker para facilitar a gestão de contêineres. Estou constantemente empenhado em aprimorar minhas habilidades e contribuir para o sucesso contínuo do projeto Coletum.'
     },
    
 ]
